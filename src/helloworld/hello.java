2de1a1002963a0e6397aa4e79b5c17c72553b09d
duyuchun <838708596@qq.com>
2017-10-17 14:26:18 +0800

    first commit

---
+++ src/helloworld/hello.java
@@ -0,0 +1,24 @@
+/*
+ *	History				Who				What
+ *  2017年10月17日			Administrator			Created.
+ */
+package helloworld;
+
+/**
+ * Title: TRS 内容协作平台（TRS WCM） <BR>
+ * Description: <BR>
+ * TODO <BR>
+ * Copyright: Copyright (c) 2004-2017 北京拓尔思信息技术股份有限公司 <BR>
+ * Company: www.trs.com.cn <BR>
+ * 
+ * @author du.yuchun
+ * @version 1.0
+ */
+public class hello {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+
+    }
+
+}
